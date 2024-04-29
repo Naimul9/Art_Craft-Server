82e347724f956c11f09453df37d5200537faf6f4
Naimul9 <naimulakib100@gmail.com>
2024-04-30 00:09:25 +0600

    send data to client by using specific id

--- index.js
+++ index.js
@@ -45,7 +45,12 @@ app.post('/addCraft', async(req, res)=>{
   res.send(result)
 })
 
-
+app.get("/myProduct/:email", async(req,res)=>{
+  console.log(req.params.email);
+  const result = await craftCollection.find({email: req.params.email}).toArray()
+  res.send(result)
+  
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
