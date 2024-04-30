2c44e22ce478d8df2fba92f98583cab805912479
Naimul9 <naimulakib100@gmail.com>
2024-04-30 15:18:49 +0600

    final submission

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 
 
-
+// mongo server
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ahphq0t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
