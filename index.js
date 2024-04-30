73fafc263167d4ef60bfc4b9d4f54e1b77762d4a
Naimul9 <naimulakib100@gmail.com>
2024-04-30 23:25:13 +0600

    final submit

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json())
 // mongo server
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ahphq0t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect()
+     
       
       const craftCollection = client.db('craftDB').collection('craft')
 
@@ -96,7 +97,7 @@ app.get('/craft/:id', async(req,res)=>{
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
